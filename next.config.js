c14a2223d74babeef8b818a761e3212fdb9a624d
jinsumin <suminjin@naver.com>
2022-10-13 15:03:11 +0900

    main component

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      "www.gravatar.com",
+      "localhost",
+      "ec2-43-200-8-24.ap-northeast-2.compute.amazonaws.com",
+      "static.toss.im",
+    ],
+  },
   reactStrictMode: true,
   swcMinify: true,
 };
--- pages/main.js
+++ pages/main.js
@@ -4,41 +4,59 @@ import Animation from "../components/animation";
 import { useEffect } from "react";
 
 const Main = () => {
+  const appStoreURL =
+    "https://static.toss.im/png-icons/timeline/applekorea.png";
+  const playStoreURL =
+    "https://static.toss.im/png-icons/timeline/googleplay.png";
+
   return (
     <div id="main-section">
       <section className="text-gray-600 body-font overflow-hidden">
-        <div className="container px-5 py-24 mx-auto justify-items-center">
-          <div className="flex flex-wrap justify-center">
-            <div className="p-12 md:w-1/2 flex flex-col items-center">
-              <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                <Image
-                  // className="object-cover object-center rounded"
-                  alt="hero"
-                  src="/hana-byuldol.png"
-                  width={680}
-                  height={700}
-                />
-              </div>
-            </div>
-            <div className="p-12 md:w-1/2 flex flex-col items-center">
-              <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                <Image
-                  // className="object-cover object-center rounded"
-                  alt="hero"
-                  src="/hana-byulsong.png"
-                  width={680}
-                  height={700}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-wrap justify-center">
-            <div className="items-center">
+        <Image
+          className="absolute inset-0 h-full w-full object-cover opacity-30"
+          layout="fill"
+          src="/hana-background.jpeg"
+          alt="background"
+        />
+        <div className="px-5 py-24 mx-auto justify-items-center">
+          <div className="mt-44 bg-orange-300 flex flex-col">
+            <h1 className="items-center mb-14 text-black text-center">
+              금융의 모든 것<br></br>토스에서 쉽고 간편하게
+            </h1>
+            <div class="flex flex-row justify-center">
               <a
-                href="#canvas-section"
-                className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+                class="border"
+                type="button"
+                aria-disabled="false"
+                target="_blank"
+                href="https://itunes.apple.com/kr/app/%ED%86%A0%EC%8A%A4/id839333328?mt=8"
+                rel="noreferrer noopener"
               >
-                <Animation width={100} height={100} />
+                <Image
+                  src={appStoreURL}
+                  width="40"
+                  height="40"
+                  className=""
+                  alt="app-store"
+                />
+                <div>App Store</div>
+              </a>
+              <a
+                class="p-button p-button--default p-button--inline p-button--fill p-button--large padding--l css-1hs38uh"
+                type="button"
+                aria-disabled="false"
+                target="_blank"
+                href="https://play.google.com/store/apps/details?id=viva.republica.toss"
+                rel="noreferrer noopener"
+              >
+                <Image
+                  src={playStoreURL}
+                  width="40"
+                  height="40"
+                  class="css-1m3khn2"
+                  alt="google-play"
+                />
+                Google Play
               </a>
             </div>
           </div>
