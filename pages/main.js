eb6dd23459242f791a9995a5434ec9df5d9b46d3
jinsumin <suminjin@naver.com>
2022-10-13 15:47:39 +0900

    qrcode generator

--- pages/main.js
+++ pages/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Animation from "../components/animation";
 import { useEffect } from "react";
+import Qrcode from "../components/qrcode";
 
 const Main = () => {
   const appStoreURL =
@@ -19,65 +20,57 @@ const Main = () => {
           alt="background"
         />
         <div className="px-5 py-24 mx-auto justify-items-center">
-          <div className="mt-44 flex flex-col items-center">
+          <div className=" flex flex-col items-center">
             <h1 className="items-center mb-14 text-black text-center text-7xl">
               금융의 모든 것<br></br>토스에서 쉽고 간편하게
             </h1>
-            <div class="flex flex-row justify-center bg-white w-1/2">
-              <div>
-                
+            <div class="flex flex-col items-center bg-white w-1/2 rounded-2xl">
+              <div className="m-10">
+                <Qrcode />
               </div>
-              <div class="flex flex-row justify-center bg-black w-52 h-12 rounded-lg m-3 shadow-lg">
-                <div>
-                  <a
-                    type="button"
-                    aria-disabled="false"
-                    target="_blank"
-                    href="https://itunes.apple.com/kr/app/%ED%86%A0%EC%8A%A4/id839333328?mt=8"
-                    rel="noreferrer noopener"
-                  >
-                    <img
-                      src={appStoreURL}
-                      width="40"
-                      height="40"
-                      alt="app-store"
-                    />
-                  </a>
+              <div className="flex flex-row">
+                <div class="flex flex-row justify-center bg-black w-52 h-12 rounded-lg m-3 shadow-lg">
+                  <div>
+                    <a
+                      type="button"
+                      aria-disabled="false"
+                      target="_blank"
+                      href="https://itunes.apple.com/kr/app/%ED%86%A0%EC%8A%A4/id839333328?mt=8"
+                      rel="noreferrer noopener"
+                    >
+                      <img
+                        src={appStoreURL}
+                        width="40"
+                        height="40"
+                        alt="app-store"
+                      />
+                    </a>
+                  </div>
+                  <div className="text-white">App Store</div>
                 </div>
-                <div className="text-white">App Store</div>
-              </div>
-              <div class="flex flex-row justify-center bg-black w-52 h-12 rounded-lg m-3 shadow-lg">
-                <div>
-                  <a
-                    type="button"
-                    aria-disabled="false"
-                    target="_blank"
-                    href="https://itunes.apple.com/kr/app/%ED%86%A0%EC%8A%A4/id839333328?mt=8"
-                    rel="noreferrer noopener"
-                  >
-                    <img
-                      src={playStoreURL}
-                      width="40"
-                      height="40"
-                      alt="app-store"
-                    />
-                  </a>
+                <div class="flex flex-row justify-center bg-black w-52 h-12 rounded-lg m-3 shadow-lg">
+                  <div>
+                    <a
+                      type="button"
+                      aria-disabled="false"
+                      target="_blank"
+                      href="https://itunes.apple.com/kr/app/%ED%86%A0%EC%8A%A4/id839333328?mt=8"
+                      rel="noreferrer noopener"
+                    >
+                      <img
+                        src={playStoreURL}
+                        width="40"
+                        height="40"
+                        alt="app-store"
+                      />
+                    </a>
+                  </div>
+                  <div className="text-white">Google Play</div>
                 </div>
-                <div className="text-white">Google Play</div>
               </div>
             </div>
           </div>
-          <div className="flex flex-wrap justify-center">
-            <div className="items-center">
-              <a
-                href="#canvas-section"
-                className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
-              >
-                <Animation width={100} height={100} />
-              </a>
-            </div>
-          </div>
-          <div className="flex flex-wrap justify-center">
+          <div className="flex flex-wrap justify-center mt-4">
             <div className="items-center">
               <a
                 href="#canvas-section"
